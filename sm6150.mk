365f4a2d94a29a52004651e40c082f2f51486447
Gagan Malvi <malvigagan@gmail.com>
2024-02-04 05:55:10 +0100

    sm6150-common: Inherit QCOM common WFD component.

--- sm6150.mk
+++ sm6150.mk
@@ -244,7 +244,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     perf \
     telephony \
     usb \
-    vibrator
+    vibrator \
+    wfd
 
 # RIL
 PRODUCT_PACKAGES += \
@@ -310,6 +311,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libwfdaac_vendor
 
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
