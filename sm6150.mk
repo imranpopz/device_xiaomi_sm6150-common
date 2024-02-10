82dba245d2f3b149faf8b8c26df87978b3b9307d
Cyber Knight <cyberknight755@gmail.com>
2024-02-10 10:42:19 +0100

    sm6150-common: Deduplicate FM packages - They're built at vendor/qcom/opensource/bluetooth/commonsys-intf.

--- sm6150.mk
+++ sm6150.mk
@@ -124,11 +124,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.emmc:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc \
     $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    qcom.fmradio
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
